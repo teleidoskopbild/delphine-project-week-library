502f1ce862869f50e412e1cb38f79b21bd746da4
Ladan-Mirzaei <161485911+Ladan-Mirzaei@users.noreply.github.com>
2024-10-22 15:00:18 +0200

    server und client

---
+++ client/src/App.jsx
@@ -0,0 +1,13 @@
+import { useState } from "react";
+
+function App() {
+  const [count, setCount] = useState(0);
+
+  return (
+    <>
+      <P>Hello Word!</P>
+    </>
+  );
+}
+
+export default App;
---
+++ server/src/app.js
@@ -0,0 +1,20 @@
+import "dotenv/config";
+import express, { json } from "express";
+import cors from "cors";
+import db from "./util/db-connect.js";
+
+const PORT = process.env.PORT || 3000;
+const app = express();
+async function getRoot(req, res) {
+  return res.json({ msg: "Hello World" });
+}
+app.use(cors());
+app.use(json());
+
+app.get("/", (req, res) => {
+  res.json({ success: true });
+});
+
+app.listen(PORT, () => {
+  console.log("api running on port " + PORT);
+});
