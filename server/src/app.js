d126717836ddba5477e41c10ac85d5b6a7f454f6
teleidoskopbild <teleidoskopbild@gmail.com>
2024-10-22 15:00:31 +0200

    initial setup data-structure

--- server/src/app.js
+++ server/src/app.js
@@ -2,17 +2,17 @@ import "dotenv/config";
 import express, { json } from "express";
 import cors from "cors";
 
-const PORT = process.env.PORT || 4444;
+const PORT = process.env.PORT || 3000;
 
 const app = express();
 
 app.use(cors());
 app.use(json());
 
-app.listen(PORT, () => {
-  console.log("api running on port " + PORT);
-});
-
 app.get("/", async (_, res) => {
   return res.json({ msg: "Hello World-Library" });
+});
+
+app.listen(PORT, () => {
+  console.log("api running on port " + PORT);
 });
