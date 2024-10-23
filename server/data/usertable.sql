a25ced5d03a9ca40640c8c4f93b06a19d77ff7b5
teleidoskopbild <teleidoskopbild@gmail.com>
2024-10-23 12:00:41 +0200

    add library_users table

---
+++ server/data/usertable.sql
@@ -0,0 +1,17 @@
+DROP TABLE IF EXISTS library_users; 
+
+-- Users Table
+CREATE TABLE library_users (
+    id SERIAL PRIMARY KEY,
+    name VARCHAR(100) NOT NULL
+);
+
+-- Insert into users table (3 users)
+INSERT INTO library_users (name) VALUES
+('Can'),
+('Ladan'),
+('Saleh'),
+('Ralf'),
+('Anastasiia');
+ 
+ SELECT * FROM library_users;
