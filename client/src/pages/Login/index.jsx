9f71ad3ecaf50402cc1d1a6d2f91ba2dd131f083
Ladan-Mirzaei <161485911+Ladan-Mirzaei@users.noreply.github.com>
2024-10-23 14:20:41 +0200

    Login Form

--- client/src/pages/Login/index.jsx
+++ client/src/pages/Login/index.jsx
@@ -1,3 +1,23 @@
 export default function Login() {
-  return <h1>Login Page</h1>;
+  // return <h1>Login Page</h1>
+  return (
+    <div className="login-container">
+      <form onSubmit={handleSubmit}>
+        <div>
+          <label htmlFor="username">Benutzername:</label>
+          <input
+            type="text"
+            id="username"
+            placeholder="Enter your name"
+            value={formData.username}
+            onChange={(e) =>
+              setFormData({ ...formData, username: e.target.value })
+            }
+            required
+          />
+        </div>
+        <button type="submit">Login</button>
+      </form>
+    </div>
+  );
 }
