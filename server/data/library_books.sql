c18ad16f53130999bb0f4e3fe78d4ea4e0403706
salehAlkarabubi <karabubi66@yahoo.com>
2024-10-24 11:08:16 +0200

    add authors-Table

--- server/data/library_books.sql
+++ server/data/library_books.sql
@@ -1,10 +1,5 @@
-DROP TABLE IF EXISTS library_users; 
+
 DROP TABLE IF EXISTS author;
--- Users Table
-CREATE TABLE library_users (
-    id SERIAL PRIMARY KEY,
-    name VARCHAR(100) NOT NULL
-);
 
 -- Create Author table
 
@@ -12,16 +7,6 @@ CREATE TABLE author (
     id SERIAL PRIMARY KEY,
     name VARCHAR(255) NOT NULL
 );
-
--- Insert into users table (3 users)
-INSERT INTO library_users (name) VALUES
-('Can'),
-('Ladan'),
-('Saleh'),
-('Ralf'),
-('Anastasiia');
- 
- SELECT * FROM library_users;
 
  -- Insert data into Author table
 INSERT INTO author ( name) VALUES
