db38c7ea25289cfa349cae2ad9fb81e9cc2bdf28
Ladan-Mirzaei <161485911+Ladan-Mirzaei@users.noreply.github.com>
2024-10-25 12:29:59 +0200

    update route borrow

--- server/src/routes/books.js
+++ server/src/routes/books.js
@@ -2,11 +2,13 @@ import { Router } from "express";
 import { getAllBooks } from "../controllers/books.js";
 import { returnBooks } from "../controllers/books.js";
 import { availableQty } from "../controllers/books.js";
+import { borrowBooks } from "../controllers/books.js";
 
 const router = Router();
 
 router.get("/", getAllBooks);
 router.post("/return", returnBooks);
+router.post("/borrow", borrowBooks);
 router.get("/available_qty", availableQty);
 
 export default router;
