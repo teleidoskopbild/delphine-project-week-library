72491e3829d333096678486d3aba32fcce9e0812
Ladan-Mirzaei <161485911+Ladan-Mirzaei@users.noreply.github.com>
2024-10-25 09:25:35 +0200

    71-Add available books

--- server/src/routes/books.js
+++ server/src/routes/books.js
@@ -1,10 +1,12 @@
 import { Router } from "express";
 import { getAllBooks } from "../controllers/books.js";
 import { returnBooks } from "../controllers/books.js";
+import { availableQty } from "../controllers/books.js";
 
 const router = Router();
 
 router.get("/", getAllBooks);
 router.post("/return", returnBooks);
+router.get("/available_qty", availableQty);
 
 export default router;
