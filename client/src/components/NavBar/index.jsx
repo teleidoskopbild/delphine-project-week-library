2bafb56869d2ba347611c5f4972950711f609c00
Ladan-Mirzaei <161485911+Ladan-Mirzaei@users.noreply.github.com>
2024-10-28 16:58:06 +0100

    css-header

--- client/src/components/NavBar/index.jsx
+++ client/src/components/NavBar/index.jsx
@@ -1,6 +1,7 @@
 import { UserContext } from "../../context/userContext.js";
 import { useContext } from "react";
 import { Link } from "react-router-dom";
+import "./NavBar.css";
 export default function NavBar() {
   const { setUsername, setUserId, username } = useContext(UserContext);
   const onLogoutClick = () => {
@@ -20,11 +21,18 @@ export default function NavBar() {
             ) : (
               <Link to="/login">Login</Link>
             )}
-
-            <Link to="/authors">Authors</Link>
-            <Link to="/">Books</Link>
-            {username && <Link to="/profile">Profile</Link>}
           </li>
+          <li>
+            <Link to="/authors">Authors</Link>
+          </li>
+          <li>
+            <Link to="/">Books</Link>
+          </li>
+          {username && (
+            <li>
+              <Link to="/profile">Profile</Link>
+            </li>
+          )}
         </ul>
       </nav>
     </header>
